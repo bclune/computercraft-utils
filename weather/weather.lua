9dbb2af3637df299d0ec40e114916f90e2719e86
Brendan <brendan@bclune.org>
2016-03-28 21:00:23 -0400

    initial commit

---
+++ weather/weather.lua
@@ -0,0 +1,23 @@
+os.loadAPI('/lib/json')
+monitor = peripheral.wrap('left')
+local forecast_url = 'https://api.forecast.io/forecast/'
+local lat = '44.475883'
+local long = '-73.212074'
+
+local api_key_file = fs.open('/etc/forecast', 'r')
+local api_key = api_key_file.readLine()
+
+local request_uri = forecast_url .. api_key .. '/' .. lat .. ',' .. long
+
+while true do
+    local response_json = http.get(request_uri).readAll()
+    local response = json.decode(response_json)
+
+    monitor.clear()
+    monitor.setCursorPos(1,1)
+    monitor.write('Burlington, VT)
+    monitor.setCursorPos(1,2)
+    monitor.write(response.currently.summary .. ", ")
+    monitor.write(response.currently.temperature .. ' F')
+    sleep(300)
+end
